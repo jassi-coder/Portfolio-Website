72ac3f1918ad5bee2c327934649f84bc2fa89256
jassi-coder <singh.jaspreet2003@gmail.com>
2026-04-30 18:19:00 +0530

    updated cv

--- portfolio_project/portfolio_project/settings.py
+++ portfolio_project/portfolio_project/settings.py
@@ -112,7 +112,7 @@ USE_TZ = True
 # -----------------------------
 # STATIC FILES
 # -----------------------------
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / "myapp/static",
