c946a38df55c0c1c388f56a09465845a790dfb3b
Jaspreet Singh <165907259+jassi-coder@users.noreply.github.com>
2026-04-30 09:39:21 +0530

    Update urls.py

--- portfolio_project/myapp/urls.py
+++ portfolio_project/myapp/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
 from . import views
+from django.contrib.sitemaps.views import sitemap
+from .sitemaps import StaticViewSitemap
+
+sitemaps = {
+    'static': StaticViewSitemap,
+}
+
 
 urlpatterns = [
     path('', views.home, name='home'),
      path('contact/', views.contact_submit, name='contact_submit'),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),  # yeh add karo
 
 ]
